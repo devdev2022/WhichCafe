42c9ec45848386c64418c5f1bc88bfa3331b62b1
Eunyoung Lee <exnyxxng@gmail.com>
2023-09-18 23:33:53 +0900

    에러핸들링 추가 및 유틸 폴더 정리

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const errorHandler = require("./utils/errorHandler");
 
 const createApp = () => {
   const app = express();
@@ -8,6 +9,7 @@ const createApp = () => {
   app.use(express.json());
   app.use(cors());
   app.use(morgan("combined"));
+  app.use(errorHandler.globalErrorHandler);
 
   return app;
 };
