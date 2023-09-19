94aaee0db32e0b88ffe4fa60cacbf9b82a6509c0
khs <devdev0226@gmail.com>
2023-09-19 22:28:51 +0900

    토큰 미들웨어 추가

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const userController = require("../controllers/userController");
+const { validateToken } = require("../utils/TokenValidation");
 
 router.post("/signup", userController.signUp);
 router.post("/signin", userController.signIn);
 
-router.get("/favorites", userController.getFavorites);
-router.post("/favorites", userController.addFavorites);
-router.delete("/favorites", userController.deleteFavorites);
+router.get("/favorites", validateToken, userController.getFavorites);
+router.post("/favorites", validateToken, userController.addFavorites);
+router.delete("/favorites", validateToken, userController.deleteFavorites);
 
 module.exports = router;
