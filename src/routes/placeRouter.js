265d9bc5e78eb749e1bf8a09adcfa6177eda8c67
khs <devdev0226@gmail.com>
2023-10-03 21:53:53 +0900

    2km 반경이내 카페 정보 조회API 구현

--- src/routes/placeRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const placeController = require("../controllers/placeController");
-
-router.get("/places", placeController.getPlaces);
-
-module.exports = router;
