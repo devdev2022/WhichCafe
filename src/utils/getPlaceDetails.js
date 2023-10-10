bfa552a135f5f8d2ece0f7a1544cf278dec44eed
khs <devdev0226@gmail.com>
2023-10-10 21:06:12 +0900

    Google_Map API 통신:사진 및 평점 통신 성공

---
+++ src/utils/getPlaceDetails.js
@@ -0,0 +1,37 @@
+require("dotenv").config({ path: "../../.env" });
+const { Client } = require("@googlemaps/google-maps-services-js");
+
+const client = new Client({});
+
+client
+  .findPlaceFromText({
+    params: {
+      input: "할리스 굽은다리역점",
+      inputtype: "textquery",
+      key: process.env.GOOGLE_MAPS_API_KEY,
+    },
+  })
+  .then((response) => {
+    const placeId = response.data.candidates[0].place_id;
+
+    return client.placeDetails({
+      params: {
+        place_id: placeId,
+        key: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    });
+  })
+  .then((response) => {
+    const details = response.data.result;
+
+    const result = {
+      photo: details.photos,
+      rate: details.rating,
+    };
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(
+      error.response ? error.response.data.error_message : error.message
+    );
+  });
