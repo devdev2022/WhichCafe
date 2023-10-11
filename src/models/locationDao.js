5498a0d9931154bce7fc55e78f1c236a9584277b
khs <devdev0226@gmail.com>
2023-10-11 16:31:32 +0900

    카페검색 추가

--- src/models/locationDao.js
+++ src/models/locationDao.js
@@ -26,8 +26,38 @@ const getNearbyAddress = async (latitude, longitude) => {
     );
     return result[queryResult];
   } catch (err) {
-    console.log(err);
     throw new Error(`GET_NEARBY_ADDRESS_ERROR`);
+  } finally {
+    conn.release();
+  }
+};
+
+const searchCafes = async (address) => {
+  const conn = await database.getConnection();
+  const queryResult = 0;
+  try {
+    const result = await conn.query(
+      `
+      SELECT 
+       cafes.id               AS cafe_id,
+       cafes.name             AS cafe_name,
+       photos.url             AS cafe_photo,
+       cafe_address.address   AS cafe_address,
+       cafe_address.latitude  AS cafe_latitude,
+       cafe_address.longitude AS cafe_longitude
+      FROM   cafe_address
+             LEFT JOIN cafes
+                    ON cafe_address.id = cafes.cafe_address_id
+             LEFT JOIN photos
+                    ON cafes.photo_id = photos.id
+      WHERE cafe_address.address LIKE CONCAT('%', ?, '%')              
+       `,
+      [address]
+    );
+    return result[queryResult];
+  } catch (err) {
+    console.log(err);
+    throw new Error(`SEARCH_CAFES_ERROR`);
   } finally {
     conn.release();
   }
@@ -35,4 +65,5 @@ const getNearbyAddress = async (latitude, longitude) => {
 
 module.exports = {
   getNearbyAddress,
+  searchCafes,
 };
