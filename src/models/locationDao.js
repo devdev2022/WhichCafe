2b2e419af4cac59df3c0f16b0c83e73eada796e0
khs <devdev0226@gmail.com>
2023-10-12 22:48:50 +0900

    자동화 구현중

--- src/models/locationDao.js
+++ src/models/locationDao.js
@@ -63,7 +63,61 @@ const searchCafes = async (address) => {
   }
 };
 
+const getAllCafeData = async () => {
+  const conn = await database.getConnection();
+  const queryResult = 0;
+  try {
+    const result = await conn.query(
+      `
+      SELECT 
+       cafes.id
+       cafes.name             
+       photos.url             
+       cafe_address.latitude  
+       cafe_address.longitude 
+      FROM   cafe_address
+             LEFT JOIN cafes
+                    ON cafe_address.id = cafes.cafe_address_id
+             LEFT JOIN photos
+                    ON cafes.photo_id = photos.id
+       `
+    );
+    return result[queryResult];
+  } catch (err) {
+    console.log(err);
+    throw new Error(`SEARCH_CAFES_ERROR`);
+  } finally {
+    conn.release();
+  }
+};
+
+const updateRate = async (ratesToUpdate) => {
+  const conn = await database.getConnection();
+  try {
+    for (let rate of ratesToUpdate) {
+      await conn.query(
+        `
+        UPDATE reviews
+        SET 
+           score = ?
+        WHERE 
+           cafe_id = ?
+        `,
+        [rate.score, rate.cafe_id]
+      );
+    }
+    return true;
+  } catch (err) {
+    console.log(err);
+    throw new Error(`UPDATE_RATE_ERROR`);
+  } finally {
+    conn.release();
+  }
+};
+
 module.exports = {
   getNearbyAddress,
   searchCafes,
+  getAllCafeData,
+  updateRate,
 };
