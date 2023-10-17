668f312ce4b3be8e9e36599004143aebb293e36e
khs <devdev0226@gmail.com>
2023-10-18 00:06:29 +0900

    2km이내 카페 불러오기 쿼리문 수정

--- src/models/locationDao.js
+++ src/models/locationDao.js
@@ -7,13 +7,16 @@ const getNearbyAddress = async (latitude, longitude) => {
     const result = await conn.query(
       `
       SELECT 
-       cafes.id               AS cafe_id,
-       cafes.name             AS cafe_name,
-       cafes.thumbnail        AS cafe_thumbnail,
-       cafe_address.address   AS cafe_address,
-       cafe_address.latitude  AS cafe_latitude,
-       cafe_address.longitude AS cafe_longitude,
-       photos.url             AS cafe_photos,
+       cafes.id                AS cafe_id,
+       cafes.name              AS cafe_name,
+       cafes.thumbnail         AS cafe_thumbnail,
+       cafe_address.address    AS cafe_address,
+       cafe_address.latitude   AS cafe_latitude,
+       cafe_address.longitude  AS cafe_longitude,
+       (CASE 
+            WHEN COUNT(photos.url) = 0 THEN JSON_ARRAY(NULL) 
+            ELSE JSON_ARRAYAGG(photos.url) 
+        END) AS cafe_photos,
        CONCAT(ROUND(St_distance_sphere(Point(cafe_address.longitude, cafe_address.latitude), Point(?, ?)) / 1000,1), 
        'km') AS distance
       FROM   cafe_address
@@ -21,12 +24,20 @@ const getNearbyAddress = async (latitude, longitude) => {
                     ON cafes.cafe_address_id = cafe_address.id
              LEFT JOIN photos
                     ON cafes.id = photos.cafe_id
+      GROUP BY 
+            cafes.id,
+            cafes.name, 
+            cafes.thumbnail, 
+            cafe_address.address, 
+            cafe_address.latitude, 
+            cafe_address.longitude
       HAVING distance <= 1
        `,
       [longitude, latitude]
     );
     return result[queryResult];
   } catch (err) {
+    console.log(err);
     throw new Error(`GET_NEARBY_ADDRESS_ERROR`);
   } finally {
     conn.release();
