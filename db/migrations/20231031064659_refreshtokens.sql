99db8ded3aa1e033c3bfe562f9a64f38fb169e9a
khs <devdev0226@gmail.com>
2023-11-20 13:18:02 +0900

    마이그레이션 이벤트 추가

--- db/migrations/20231031064659_refreshtokens.sql
+++ db/migrations/20231031064659_refreshtokens.sql
@@ -6,12 +6,21 @@ CREATE TABLE refreshtokens (
     refresh_token   VARCHAR(255)   NOT NULL,
     device_info     VARCHAR(255),
     created_at      TIMESTAMP      NOT NULL DEFAULT current_timestamp,
-    expires_at      TIMESTAMP      NOT NULL,
+    expires_at      TIMESTAMP      NOT NULL DEFAULT (current_timestamp + INTERVAL 14 DAY),
 
     PRIMARY KEY(id),
     CONSTRAINT fk_refreshtokens_users FOREIGN KEY(user_id)
         REFERENCES users (id) ON DELETE CASCADE ON UPDATE RESTRICT
 );
 
+CREATE TRIGGER before_refreshtoken_insert
+BEFORE INSERT ON refreshtokens
+FOR EACH ROW
+BEGIN
+   SET NEW.expires_at = CURRENT_TIMESTAMP + INTERVAL 14 DAY;
+END;
+
+
 -- migrate:down
-DROP TABLE refreshtokens
+DROP TRIGGER IF EXISTS before_refreshtoken_insert;
+DROP TABLE IF EXISTS refreshtokens;
