1c9f7f384236d423e29bc866264b5cb38bddf356
khs <devdev0226@gmail.com>
2023-11-26 11:27:31 +0900

    CORS 설정 추가

--- app.js
+++ app.js
@@ -18,7 +18,16 @@ const createApp = () => {
 
   app.use(cookieParser());
 
-  app.use(cors());
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:3000",
+        "http://cafeeodi.com",
+        "http://www.cafeeodi.com",
+      ],
+      credentials: true,
+    })
+  );
   app.use(express.json());
   app.use(morgan("combined"));
 
