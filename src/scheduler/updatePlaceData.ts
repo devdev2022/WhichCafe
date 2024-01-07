89f4226d01578710f6af2d67a45ae1fc5d0f6f8f
khs <devdev0226@gmail.com>
2024-01-07 20:01:52 +0900

    시간 재설정

--- src/scheduler/updatePlaceData.ts
+++ src/scheduler/updatePlaceData.ts
@@ -22,7 +22,7 @@ interface Cafe {
 async function main(): Promise<void> {
   try {
     const allCafes = (await locationDao.getAllCafeData()) as Array<Cafe>;
-    console.log(allCafes);
+
     let ratesToUpdate: RateUpdate[] = [];
 
     const allTasks = allCafes.slice(0, 10).map(async (cafe) => {
@@ -33,7 +33,7 @@ async function main(): Promise<void> {
 
       try {
         placeId = await googleMapsClient.getPlaceId(cafeName);
-        console.log(placeId);
+
         if (!placeId) {
           console.error(`No location data found for cafe ${cafeName}`);
           return null;
@@ -174,7 +174,7 @@ async function main(): Promise<void> {
   }
 }
 
-const scheduledTask = schedule.scheduleJob("00 50 19 7 * *", async function () {
+const scheduledTask = schedule.scheduleJob("00 10 11 7 * *", async function () {
   await main();
 });
 
